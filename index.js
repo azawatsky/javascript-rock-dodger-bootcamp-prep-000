7d62c73687d12fa3ed934db427e0dab26d9eff0a
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-07-18 16:29:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
-  window.requestAnimationFrame({
+  window.requestAnimationFrame(function(){
     left = positionToInteger(DODGER.style.left);
     if (left > 0) {
       DODGER.style.left = `${left - 4}px`;
