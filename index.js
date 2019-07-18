38955cbe1a8662735b28247491aa45753bd9eabc
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-07-18 04:25:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  gameInterval="";
+  delete gameInterval;
   while (ROCKS[0]){
     GAME.removeChild(ROCKS[0])
   }
